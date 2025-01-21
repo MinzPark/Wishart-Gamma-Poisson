10c3f6cd0459ec89ddbcc8a82eb3c22fabb0a90a
MinzPark <114046169+MinzPark@users.noreply.github.com>
2025-01-20 22:22:45 -0500

    Update simulation_wishart.R

--- simulation_wishart.R
+++ simulation_wishart.R
@@ -5,25 +5,39 @@
 # generate data
 #############################################
 
-
+filepath <- "/Users/baengminji/Desktop"
+filename = file.path(filepath,'wishart/result',gsub("-", "", Sys.Date()))
+ifelse(!file.exists(filename), dir.create(filename, recursive = TRUE) ,0)
 # S: scale param pxp
 # nu: degree freedom
 
 
 set.seed(1)
-N <- 100000
+N <- 50000
 d <- 3
+
+
 
 
 #-------------------------------------------
 # 1) S with d=4
 #-------------------------------------------
 
-delta <- 4
-S <- matrix(c( 0.25, 0.15,  0.15, 
-                0.15,  0.25, 0.15, 
-                0.15, 0.15, 0.25), nrow = 3, byrow = TRUE)
+delta <- 3
+# S <- matrix( c( 0.25, 0.15,  0.15,
+#                 0.15,  0.25, -0.15,
+#                  0.15, -0.15, 0.25), nrow = 3, byrow = TRUE)
+S <- matrix( c( 1/3, 0.15,  0.1,
+                0.15,  1/3, -0.2,
+                 0.1, -0.2, 1/3), nrow = 3, byrow = TRUE)
 
+eigen(S)
+eigen(abs(S))
+
+# delta <- 4
+# S <- matrix(1/4 * c(1, 0.9,  0.8,
+#                 0.9,  1, 0.7,
+#                 0.8, 0.7, 1), nrow = 3, byrow = TRUE)
 
 
 #lambs <- rep(c(0.3,0.6,0.9),N)
@@ -53,6 +67,8 @@ table(Ns)
 N.vec = Ns
 Ns <- matrix(N.vec, nrow = N, byrow = TRUE)
 
+write.csv(N.vec,'Ns.csv')
+
 
 #############################################
 # optim off- diag elt
@@ -79,7 +95,7 @@ nb_ll <- function(lambs, N.vec, param){
 
 
 
-delta.hat <- optim(par = c(0), fn = nb_ll, N.vec = N.vec, lambs = lambs, method = 'Brent', lower = 0, upper = 10)$par
+delta.hat <- optim(par = c(0), fn = nb_ll, N.vec = N.vec, lambs = lambs, method = 'Brent', lower = delta, upper = 10)$par
 diag(Sig_hat) <- 1/delta.hat
 
 
@@ -114,7 +130,7 @@ delta_f <- function( params, Lambs, Ns){
 
 
 (delta.hat <- optim(par = c(0), fn = delta_f, Ns = Ns, Lambs = Lambs,
-                    method = 'Brent', lower = 0, upper = 100)$par)
+                    method = 'Brent', lower = delta, upper = 100)$par)
 diag(Sig_hat) <- 1/delta.hat
 
 
@@ -177,7 +193,6 @@ off_diag_elt <- function(param, j1, j2, Ns, Lamb, delta){
 
 
 
-
 # pairwise estimation index
 combination <-c()
 for(i in 1:(d-1)){
@@ -212,38 +227,37 @@ end.time <- Sys.time()
 
 
 Sig_ll <- function(M, Lambs, Ns, delta.hat, Sig_hat){
-  if(min(eigen(Sig_hat)$value < 0) > 0){
+  if(sum(eigen(Sig_hat)$value < 0) > 0){
     return("Some eigen value is neg")
-  }else{  
+  }else{
     l_total = 0
     # Sigma : -179408.2  Sgn * Sigma -358873.8
     start.time <- Sys.time()
-    
+
     Us <- c()
     d <- dim(Sig_hat)[1]
     for(j in 1:M){
-      #U <- diag(rwishart(delta.hat, Sig_hat))
-      U <- diag(dlm::rwishart(delta.hat, p = nrow(Sig_hat),Sig_hat, SqrtSigma = diag(p)))
+      U <- diag(LaplacesDemon::rwishart(delta.hat, Sig_hat))
+      #U <- diag(dlm::rwishart(delta.hat, p = nrow(Sig_hat),Sig_hat, SqrtSigma = diag(p)))
       Us <- c(Us, U)
     }
     Us.mat <- matrix(Us, nrow = M, byrow = TRUE)
-    
+
     for(k in 1:N){
-      
+
       lamb <- Lambs[k,]
       N.p <- Ns[k,]
       ll = exp(-apply(sweep(Us.mat, 2, lamb, `*`),1,sum)) * apply(sweep(Us.mat, 2, N.p, `^`),1, function(x) prod(x))
-      
+
       l_total = l_total + log(mean(ll))
     }
-    
+
     end.time <- Sys.time()
     execution.time <- end.time - start.time
     print(execution.time)
-    
+
     return(l_total)}
 }
-
 
 
 
@@ -282,20 +296,20 @@ Sign.scen <- function(d, Sigma){
 
 
 
-
 ####################
 
 
-
+# delta.hat <- 3
+# Sig_hat <- S
 
 #m.set <- Sign.scen(d, S)
+
 rep = 10
 tmp <- c()
 
+m.set <- Sign.scen(d, Sig_hat)
 for(scn in 1:rep){
-  m.set <- Sign.scen(d, Sig_hat)
-  
-  M<-1000
+  M<-10000
   sig.l.val <- c()
   i <- 1
   
@@ -307,30 +321,77 @@ for(scn in 1:rep){
     cat("\n")
   }
   tmp <- rbind(tmp, sig.l.val)
+  cat(cbind(tmp, tmp[,1] - tmp[,2]))
 }
 
 
-result <- cbind(seq(1,rep),tmp,tmp[,1] - tmp[,2])
-colnames(result) <- c('iter',"Matrix1", "Matrix2", 'difference')
+result <- tryCatch({
+  result <- cbind(seq(1, rep), tmp, tmp[,2] - tmp[,1])
+  colnames(result) <- c('iter', "Matrix1", "Matrix2", 'difference')
+  result
+}, error = function(e) {
+  result <- cbind(seq(1, rep), tmp)
+  colnames(result) <- c('iter', "Matrix1", "Matrix2")
+  result
+})
+
+
 
 # save the result for Sigma scenario and likelihood ftn of each Sigma
 # save sigma scenario
-sigma_scenario <- "Sigma Scenario:\n"
-for (i in seq_along(m.set)) {  
-  sigma_scenario <- paste0(
-    sigma_scenario, 
-    "Matrix ", i, ":\n", 
-    paste(capture.output(print(m.set[[i]])), collapse = "\n"), 
-    "\n\n"  
-  )
-}
-writeLines(sigma_scenario, paste0(filename,"/Sim_sigma_scenario.txt"))
+# sigma_scenario <- c()
+# sigma_scenario <- "Sigma Scenario:\n"
+# for (i in seq_along(m.set)) {
+#   sigma_scenario <- paste0(
+#     sigma_scenario,
+#     "Matrix ", i, ":\n",
+#     paste(capture.output(print(m.set[[i]])), collapse = "\n"),
+#     "\n\n"
+#   )
+# }
+write.csv(result, paste0(filename,'/Test_Sim_result.csv'), row.names = FALSE)
 
-# save the result of Simulation
-filename = file.path(path,'wishart/result',gsub("-", "", Sys.Date()))
-ifelse(!file.exists(filename), dir.create(filename, recursive = TRUE) ,0)
+# plot the joint log-likelihood to compare the performance between Sigma1 and Sigma2
+# save the plot of log-likelihood for estimated Simulation
+png(paste0(filename,"/Sim_comparison_plot.png"), width = 600, height = 500) 
+par(mgp = c(10, 0.5, 0))  
+par(mar = c(4, 4, 4, 2))
+par(oma = c(1,1,1,1))
 
-write.csv(result, paste0(filename,'/Sim_result.csv'), row.names = FALSE)
+range_x <- range(plot.mat[,2])
+range_y <- range(plot.mat[,1])
+
+extended_range <- range(c(range_x, range_y))
+
+x_axis_tick <- seq(from = extended_range[1], to = extended_range[2], length.out = 3)
+
+y_axis_tick <- seq(from = extended_range[1], to = extended_range[2], length.out = 3)
+x_axis_tick <- floor(x_axis_tick)
+y_axis_tick <- floor(y_axis_tick)
+
+
+plot(x = plot.mat[,2], y = plot.mat[,1],
+     xlim = extended_range, ylim = extended_range,
+     xlab = expression(Sigma[1]), 
+     ylab = expression(Sigma[2]), 
+     main = expression("The joint log-likelihood between " * Sigma[1] * " and " * Sigma[2]),
+     cex.main = 1.5, axes = FALSE)
+
+
+abline(a = 0, b = 1, col = "black", lwd = 2, lty = 2)
+
+axis(side = 1, at = x_axis_tick, labels = round(x_axis_tick, 2), cex.axis = 1.3,lwd = 0.5, las = 1)
+axis(side = 2, at = y_axis_tick, labels = round(y_axis_tick, 2), cex.axis = 1.3, lwd= 0.5, las = 3)
+
+mtext(expression(Sigma[1]), side = 1, line = 2.5, las = 1, cex = 1.8)
+mtext(expression(Sigma[2]), side = 2, line = 2.5, las = 1, cex = 1.8)
+box(lwd = 1, col = "black")
+dev.off()
+
+
+# save the estimated Sigma
+writeLines(sigma_scenario, paste0(filename,"/Test_Sim_sigma_scenario.txt"))
+
 
 
 
